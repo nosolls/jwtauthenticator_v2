1de83d155ede57f16247efd246aafdd49ff9cdfc
Noah Samuel Oller Smith <nollersm@purdue.edu>
2025-08-06 17:41:07 -0600

    Update reference info

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     name='jupyterhub-jwtauthenticator-v2',
     version='2.0.3',
     description='JSONWebToken Authenticator for JupyterHub',
-    url='https://github.com/izihawa/jwtauthenticator_v2',
-    author='ppodolsky',
-    author_email='ppodolsky@me.com',
+    url='https://github.com/I-GUIDE/iguide-jupyterhub/jwtauthenticator_v2',
+    author='nosolls',
+    author_email='noahsoller@gmail.com',
     license='Apache 2.0',
     packages=['jwtauthenticator'],
     install_requires=[

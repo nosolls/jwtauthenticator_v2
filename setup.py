fda6c4eb4a3920270f2ffc489a5c40b313d80fe0
Noah Samuel Oller Smith <nollersm@purdue.edu>
2025-08-06 17:43:37 -0600

    Fix URL

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='jupyterhub-jwtauthenticator-v2',
     version='2.0.3',
     description='JSONWebToken Authenticator for JupyterHub',
-    url='https://github.com/I-GUIDE/iguide-jupyterhub/jwtauthenticator_v2',
+    url='https://github.com/I-GUIDE/iguide-jupyterhub/jupyter/jwtauthenticator_v2',
     author='nosolls',
     author_email='noahsoller@gmail.com',
     license='Apache 2.0',
